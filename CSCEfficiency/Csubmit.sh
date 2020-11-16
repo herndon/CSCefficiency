8a08adfa72c81e2a0aaed18e18f91778cfaeb6fa
Matthew Herndon <herndon@hep.wisc.edu>
2020-11-16 15:36:53 -0600

    Delete Csubmit.sh

--- CSCEfficiency/Csubmit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "Submitting Condor Job(s)"
-
-farmoutAnalysisJobs --input-files-per-job=1 --input-runs=319841-319851 --extra-inputs=$CMSSW_BASE/src/CSCEfficiency/CSCEfficiency/Cert_314472-319851_13TeV_PromptReco_Collisions18_JSON.txt --input-dbs-path=/SingleMuon/Run2018C-ZMu-PromptReco-v3/RAW-RECO --skip-existing-output CSCeff_2018C-v3_Aug-9-2018_319841-319851_skim $CMSSW_BASE $CMSSW_BASE/src/CSCEfficiency/CSCEfficiency/CondorSubmit.py 'inputFiles=$inputFileNames' 'outputFile=$outputFileName' --assume-input-files-exist --vsize-limit=7000 &
