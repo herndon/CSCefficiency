59fcd4bad6f5c22f5ae7ed782869213782fb1eee
Matthew Herndon <matthew.herndon@cern.ch>
2022-07-26 09:52:06 -0500

    Run 3 Data py scripts

---
+++ CSCEfficiency/crabConfig_CSCEff_v1_2022C.py
@@ -0,0 +1,23 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = 'CSCEff2022C_2'
+config.General.workArea = ''
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'create_ntuple_crab_Run3Data_2022C.py'
+config.JobType.maxMemoryMB = 2500 
+config.JobType.allowUndistributedCMSSW = True
+
+config.Data.inputDataset = '/SingleMuon/Run2022C-ZMu-PromptReco-v1/RAW-RECO'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1 
+#config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions18/13TeV/Era/Prompt/Cert_320394-325273_13TeV_PromptReco_Collisions18_JSON_eraD.txt'
+#config.Data.runRange = '320394-325273' # '316998-319312 320394-325273 320394-321500 322348-322355'
+config.Data.publication = True
+config.Data.outputDatasetTag = 'CSCEff2022C_20220725_2'
+
+config.Site.storageSite = 'T2_US_Wisconsin' 
