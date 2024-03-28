7b1daf41a64bc444442baad27318389ad20480e8
Matthew Herndon <matthew.herndon@cern.ch>
2024-03-28 09:57:38 -0500

    Update 1 for Run 3 2024

--- CSCEfficiency/interface/TPTrackMuonSys.h
+++ CSCEfficiency/interface/TPTrackMuonSys.h
@@ -31,7 +31,7 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/InputTag.h"
-#include "FWCore/Framework/interface/EDAnalyzer.h"
+//#include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
