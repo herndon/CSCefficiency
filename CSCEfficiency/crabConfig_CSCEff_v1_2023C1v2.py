1e2597d6d9ddaeeba5ec379dd7a3460b9fb4aae5
Justin Marquez <justin.marquez@wisc.edu>
2024-06-22 18:47:49 -0500

    add crab files and update README and gitignore. change makefile.txt to makefile

---
+++ CSCEfficiency/crabConfig_CSCEff_v1_2023C1v2.py
@@ -0,0 +1,24 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = 'CSCEff2023C1_5'
+config.General.workArea = ''
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'create_ntuple_crab_Run3Data_2023C1v2_v1.py'
+config.JobType.maxMemoryMB = 2500 
+config.JobType.allowUndistributedCMSSW = True
+
+config.Data.inputDataset = '/Muon1/Run2023C-ZMu-PromptReco-v2/RAW-RECO'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 4 
+#config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions18/13TeV/Era/Prompt/Cert_320394-325273_13TeV_PromptReco_Collisions18_JSON_eraD.txt'
+#config.Data.runRange = '320394-325273' # '316998-319312 320394-325273 320394-321500 322348-322355'
+#config.Data.runRange = '367071-367081'
+config.Data.publication = True
+config.Data.outputDatasetTag = 'CSCEff2023C1_2023_0512_3'
+
+config.Site.storageSite = 'T2_US_Wisconsin' 
