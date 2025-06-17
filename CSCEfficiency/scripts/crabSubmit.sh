c40586d98e43762cf76340bdb4efd1b22f1df771
Justin Marquez <jmarquez1014@gmail.com>
2025-06-17 17:08:28 +0200

    add usage printout to script when run without any arguments

--- CSCEfficiency/scripts/crabSubmit.sh
+++ CSCEfficiency/scripts/crabSubmit.sh
@@ -1,3 +1,11 @@
+if [[ -z $1 ]]; then
+  echo "usage: $0 JOB ..."
+  echo
+  echo " JOB: the name of a section in settings.cfg with the desired options for CRAB job"
+  echo " ...: additional section(s)"
+  exit 1
+fi
+
 while [[ $# -gt 0 ]]; do
   if [[ ! -z $1 && -f settings.cfg ]]; then
     if [[ -z $(grep "^\s*\[$1\]\s*$" settings.cfg) ]]; then
